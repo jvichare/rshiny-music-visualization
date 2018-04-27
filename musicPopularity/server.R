7983eb556af40dd91d403ec160f4ecfa3448ef9d
jvichare <joshvichare@gmail.com>
2018-04-27 16:32:49 -0400

    changes to accept user input for most graphs, filled out missing data tables

--- musicPopularity/server.R
+++ musicPopularity/server.R
@@ -14,37 +14,49 @@ library(ggplot2)
 
 shinyServer(function(input, output) {
   
-  ## Reordering the dataset columns for the DT::renderDataTable object
+  ## Reordering the dataset columns for the full dataset object
   music_table <- all_music[, c(22, 15, 14, 17, 23, 3, 2, 4, 7)]
   
+  ## Reodering the dataset columns for the threshold dataset object
+  median_stats <- median_stats[, c(1, 15, 19, 5, 3, 4, 8, 9, 10, 6, 13)]
+  
   output$boxplot <- renderPlotly({
-    plot_ly(all_music, y = ~popularity, color = ~genre, type = "box") %>% 
-      layout(title = "Popularity by Genre Box Plot")
+    plot_ly(all_music, y = ~get(input$plot_var), color = ~genre, type = "box") %>% 
+      layout(yaxis = list(title = ""))
   })
   
   output$densityplot <- renderPlot({
-    ggplot(data = all_music, aes(popularity)) +
+    ggplot(data = all_music, aes_string(input$plot_var)) +
       geom_density(size = 0.75, alpha = 0.1, aes(fill = genre, color = genre)) +
       theme_bw() +
-      ggtitle("Density of Popularity by Genre")
+      labs(x = "",
+           y = "")
   })
   
   output$threeDscatter <- renderPlotly({
-    plot_ly(subset_all_music, x = ~danceability, y = ~acousticness, z = ~popularity,
-            color = ~genre, height = 700, width = 1000,
-            text = ~paste('Artist:', artist_name, '<br>Song:', song_name, 
-                          '<br>Danceability:', danceability,'<br>Acousticness:', energy, 
-                          '<br>Pop.:', popularity)) %>%
+    plot_ly(subset_all_music, x = ~get(input$scatter_var1), y = ~get(input$scatter_var2), z = ~popularity,
+            color = ~genre, size = ~popularity, key = ~full_name, height = 650,
+            text = ~paste('Artist:', artist_name, '<br>Song:', song_name, '<br>Popularity:', popularity)) %>%
       add_markers() %>%
-      layout(scene = list(xaxis = list(title = 'Danceability'),
-                          yaxis = list(title = 'Acousticness'),
-                          zaxis = list(title = 'Popularity')))
+      layout(scene = list(zaxis = list(title = 'Popularity'),
+                          xaxis = list(title = input$scatter_var1),
+                          yaxis = list(title = input$scatter_var2)))
     
+  })
+  
+  output$click <- renderPrint({
+    d <- event_data("plotly_click")
+    if (is.null(d)) "Click to save the song information" else d
   })
   
   output$table <- DT::renderDataTable({
     datatable(music_table, rownames = F)
   })
+  
+  output$threshold <- DT::renderDataTable({
+    datatable(median_stats, rownames = F)
+  })
+  
   
   
 }
