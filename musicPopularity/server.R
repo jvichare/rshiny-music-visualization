ac02caa3db1b0951b042a48d30c829041bcd4fff
jvichare <joshvichare@gmail.com>
2018-04-27 12:23:20 -0400

    added initial plots for 2D/3D and DT

--- musicPopularity/server.R
+++ musicPopularity/server.R
@@ -12,7 +12,41 @@ library(plotly)
 library(dplyr)
 library(ggplot2)
 
-# Define server logic required to draw a histogram
 shinyServer(function(input, output) {
+  
+  ## Reordering the dataset columns for the DT::renderDataTable object
+  music_table <- all_music[, c(22, 15, 14, 17, 23, 3, 2, 4, 7)]
+  
+  output$boxplot <- renderPlotly({
+    plot_ly(all_music, y = ~popularity, color = ~genre, type = "box") %>% 
+      layout(title = "Popularity by Genre Box Plot")
+  })
+  
+  output$densityplot <- renderPlot({
+    ggplot(data = all_music, aes(popularity)) +
+      geom_density(size = 0.75, alpha = 0.1, aes(fill = genre, color = genre)) +
+      theme_bw() +
+      ggtitle("Density of Popularity by Genre")
+  })
+  
+  output$threeDscatter <- renderPlotly({
+    plot_ly(subset_all_music, x = ~danceability, y = ~acousticness, z = ~popularity,
+            color = ~genre, height = 700, width = 1000,
+            text = ~paste('Artist:', artist_name, '<br>Song:', song_name, 
+                          '<br>Danceability:', danceability,'<br>Acousticness:', energy, 
+                          '<br>Pop.:', popularity)) %>%
+      add_markers() %>%
+      layout(scene = list(xaxis = list(title = 'Danceability'),
+                          yaxis = list(title = 'Acousticness'),
+                          zaxis = list(title = 'Popularity')))
     
-})
+  })
+  
+  output$table <- DT::renderDataTable({
+    datatable(music_table, rownames = F)
+  })
+  
+  
+}
+)
+
