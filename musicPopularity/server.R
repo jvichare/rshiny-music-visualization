dc2b9ed1cc0135b68635bfe4673b3c7c6420db5c
jvichare <joshvichare@gmail.com>
2018-04-27 10:45:03 -0400

    cleanup default server.R

--- musicPopularity/server.R
+++ musicPopularity/server.R
@@ -10,22 +10,9 @@
 library(shiny)
 library(plotly)
 library(dplyr)
-library(data.table)
 library(ggplot2)
-library(plotly)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-     
-    output$distPlot <- renderPlot({
-      
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2] 
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-        
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-      
-    })
     
 })
